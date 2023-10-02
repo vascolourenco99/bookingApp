63a5a2892fcd6dc71b00269c44fee6c90f7aa006
vascolourenco <vascocorreia99@gmail.com>
2023-10-02 11:40:27 +0100

    api keys

--- firebase.js
+++ firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyClIlfaNAn-tDyLErPlTHrIm6UUJ3WK4dg",
-  authDomain: "booking-project-54a99.firebaseapp.com",
-  projectId: "booking-project-54a99",
-  storageBucket: "booking-project-54a99.appspot.com",
-  messagingSenderId: "135868483742",
-  appId: "1:135868483742:web:d0d77fb8042a949924d38f"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "1"
 };
 
 
